6e7a888065740046fd3673630000c866268d4d9a
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2019-07-25 06:44:46 +0530

    login page

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
-import app.views
+from app.views import *
 
 urlpatterns = [
-    path('', app.views.index, name=''),
+    path('', auth_views.LoginView.as_view(template_name = 'app/registration/login.html'), name='login'),
 ]
