bda4a5e9ccd940acf5b1be22686732a191e09c70
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2019-08-13 10:41:33 +0530

    modified

--- app/records_helper.py
+++ app/records_helper.py
@@ -22,41 +22,50 @@ def insert_into_db(file_path):
     # Loop will print all columns name 
     for row in range(2, total_rows + 1): 
 
-        category_insertion(sheet_obj.cell(row = row, column = 1).value)
-        sub_category_insertion(sheet_obj.cell(row = row, column = 1).value)
-
-        for col in range(1, total_columns + 1): 
+        category_insertion(sheet_obj.cell(row = row, column = 1).value, sheet_obj.cell(row = row, column = 2).value)
+       
+        for col in range(3, total_columns + 1): 
             cell_obj = sheet_obj.cell(row = row, column = col) 
           
     print(msg)
 
 
-def category_insertion(category):
+#===========================================================================================
+# Category & Sub Category Insertion   
+# ==========================================================================================
+# 
+
+def category_insertion(category, sub_category):
+    
+    cat = None
+
     try:
         cat_insert = Category(
             category_name = category.strip(),
             category_is_parent = True,
         )
 
-        cat_insert.save()
+        cat = cat_insert.save()
+    
     except IntegrityError:
-        pass
+        cat = Category.objects.get(category_name = category.strip())
+
+    # Sub category   
+    # 
         
+    if cat is not None:
+        sub_category = sub_category.strip()
 
-def sub_category_insertion(category, sub_category):
+        try:
+            cat_insert = Category(
+                category_name = sub_category.strip(),
+                category_is_parent = False,
+                category_parent_id = cat,
+            )
+            cat_insert.save()
+        except IntegrityError:
+            pass
 
-    #category = category.upper().strip()
-
-    cat = Category.objects.filter(category_name__contains = category)
-
-    if a == b:
-        pass
-    try:
-        cat_insert = Category(
-            category_name = category.strip(),
-            category_is_parent = True,
-        )
-
-        cat_insert.save()
-    except IntegrityError:
-        pass
+#====================================================================================
+#   Brands Insertion
+#====================================================================================
